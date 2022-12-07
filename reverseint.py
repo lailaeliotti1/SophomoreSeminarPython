c1a139ab1312260974016cda08b7ebe104eb147f
lailaeliotti1 <93938274+lailaeliotti1@users.noreply.github.com>
2022-12-07 09:55:19 -0600

    Add files via upload

---
+++ reverseint.py
@@ -0,0 +1,9 @@
+def reverse_int(num):
+    reversed_num = 0
+
+    while num != 0:
+        digit = num % 10
+        reversed_num = reversed_num * 10 + digit
+        num //= 10
+
+    return "Reversed Number: " + str(reversed_num)
